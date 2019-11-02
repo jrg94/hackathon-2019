5493f8f3626ea21c8fb6acfb833a21cecfff44f8
ryanbentivegna\ryan bentivegna <ryanabentivegna@gmail.com>
2019-11-02 14:46:04 -0400

    Working group, many bad data points that are counted as reliable

--- vis.js
+++ vis.js
@@ -1,21 +1,30 @@
 d3.csv("data/HackathonDataDaily.csv").then(plot)
 
-
-
 function plot(dataset){
+	
+	isolateBuilding(dataset, "Oxley Electric Meter")
+	sort(dataset)
+	console.log(dataset)
+	//clean(dataset)
+	
 	console.log("Hello World!")
 	console.log(dataset)
 	var w = 600
 	var h = 400
 	var padding = 40
 	
+	console.log(d3.extent(dataset, (d) => new Date(d.Time)))
+	console.log(d3.extent(dataset, (d) => parseFloat(d.CurrentValue)))
+	
 	var xScale = d3.scaleTime()
 		.domain(d3.extent(dataset, (d) => new Date(d.Time)))
 		.range([padding, w - padding * 2])
 	
 	var yScale = d3.scaleLinear()
-		.domain(d3.extent(dataset, (d) => Number(d.CurrentValue)))
+		.domain(d3.extent(dataset, (d) => parseFloat(d.CurrentValue)))
 		.range([h - padding, padding])
+		
+	var colorScale = d3.scaleOrdinal(d3.interpolateInferno)
 	
 	var xAxis = d3.axisBottom(xScale).ticks(10)
 	
@@ -31,9 +40,9 @@ function plot(dataset){
 		.enter()
 		.append("circle")
 		.attr("cx", (d) => xScale(new Date(d.Time)))
-		.attr("cy", (d) => h - yScale(Number(d.CurrentValue)))
+		.attr("cy", (d) => h - yScale(parseFloat(d.CurrentValue)))
 		.attr("r", 5)
-		.attr("fill", "green")
+		.attr("fill", (d) => parseFloat(d.CurrentValue) > 0 ? "green" : "red")
 		
 	svg.append("g")
 		.attr("class", "x axis")
@@ -45,3 +54,37 @@ function plot(dataset){
 		.attr("transform", "translate(" + padding + ", 0)")
 		.call(yAxis)
 }
+
+function isolateBuilding(dataset, name){
+	for(var i = 0; i < dataset.length; i++){
+		var obj = dataset[i]
+		if(obj.BuildingID != name){
+			dataset.splice(i, 1)
+			i--
+		}
+	}
+}
+
+function clean(dataset){
+	for(var i = 0; i < dataset.length; i++){
+		var obj = dataset[i]
+		if(parseFloat(obj.CurrentValue) < 0 || obj.Status != "OK" || obj.CurrentValue > 1e+8){
+			dataset.splice(i, 1)
+			i--
+		}
+	}
+}
+
+function sort(dataset){
+	dataset.sort((b, a) => Number(a.CurrentValue) - Number(b.CurrentValue))
+	
+	
+}
+
+
+
+
+
+
+
+
