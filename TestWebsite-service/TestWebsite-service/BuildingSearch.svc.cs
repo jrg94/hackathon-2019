e59bb88d6594855649da0a976da400f05ec06973
hoBBlers <calving159@gmail.com>
2019-11-02 17:48:05 -0400

    Add files via upload

---
+++ TestWebsite-service/TestWebsite-service/BuildingSearch.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace TestWebsite_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BuildingSearch" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BuildingSearch.svc or BuildingSearch.svc.cs at the Solution Explorer and start debugging.
+    public class BuildingSearch : IBuildingSearch
+    {
+        public String[] buildingConstructor()
+        {
+            String[] buildings = new string[216];
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader("Hack.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                buildings[counter] = line.Substring(1, line.Length);
+                counter++;
+            }
+            return buildings;
+        }
+    }
+}
+
---
+++ TestWebsite-service/TestWebsite-service/IBuildingSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TestWebsite_service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBuildingSearch" in both code and config file together.
+    [ServiceContract]
+    public interface IBuildingSearch
+    {
+        [OperationContract]
+        String[] buildingConstructor();
+
+    }
+}
