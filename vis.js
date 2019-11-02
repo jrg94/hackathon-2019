2650e768dd075f02f7aac9877e95813e816a21fe
ryanbentivegna\ryan bentivegna <ryanabentivegna@gmail.com>
2019-11-02 17:14:59 -0400

    Everything in graph is good, only need axis labels and the legend's box

--- vis.js
+++ vis.js
@@ -41,8 +41,12 @@ function plot(dataset){
 		.append("svg")
 		.attr("width", w)
 		.attr("height", h)
-		
-	svg.selectAll("circle")
+
+	
+	
+	
+	svg.append("g")
+		.selectAll("circle")
 		.data(dataset)
 		.enter()
 		.append("circle")
@@ -60,6 +64,44 @@ function plot(dataset){
 		.attr("class", "y axis")
 		.attr("transform", "translate(" + padding + ", 0)")
 		.call(yAxis)
+		.selectAll("text")
+		.style("text-anchor", "middle")
+		.attr("dx", "1em")
+		.attr("dy", "-1em")
+		.attr("transform", "rotate(-90)")
+	
+	legend = svg.append("g")
+	
+	
+	legend.append("rect")
+		.attr("fill", "white")
+		.attr("stroke", "black")
+		.attr("height", 15 + 25 * includedBuildings.length)
+		.attr("width", 100)//getMaxTextWidth(includedBuildings))
+	
+	legend.selectAll("mydots")
+	  .data(includedBuildings)
+	  .enter()
+	  .append("circle")
+		.attr("cx", 20)
+		.attr("cy", function(d,i){ return 20 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+		.attr("r", 7)
+		.style("fill", function(d){ return colorScale(d)})
+
+	// Add one dot in the legend for each name.
+	legend.selectAll("mylabels")
+	  .data(includedBuildings)
+	  .enter()
+	  .append("text")
+		.attr("x", 40)
+		.attr("y", function(d,i){ return 20 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+		.style("fill", function(d){ return colorScale(d)})
+		.text(function(d){ return d})
+		.attr("text-anchor", "left")
+		.style("alignment-baseline", "middle")
+	
+		
+	
 }
 
 /**
@@ -100,7 +142,27 @@ function sortByCurrentValue(dataset){
 
 
 
+/*function getMaxTextWidth(textArray){
+	var max = [0, 0]
+	for(var i = 0; i < textArray.length; i++){
+		var size = textSize(textArray[i])
+		if(size["width"] > max["width"]){
+			max = size
+		}
+	}
+	return max[0]
+}
+
+// Usage: textSize("This is a very long text"); 
+// => Return: Object {width: 140, height: 15.453125}
+function textSize(text) {
+    var container = d3.select('body').append('svg');
+    container.append('text').attr({ x: -99999, y: -99999 }).text(text);
+    var size = container.node().getBBox();
+    container.remove();
+    return { width: size.width, height: size.height };
+}
 
 
 
-
+*/
