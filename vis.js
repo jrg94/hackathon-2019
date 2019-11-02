79ee54cb7fbb6b936797071e050e51e0d58eea75
ryanbentivegna\ryan bentivegna <ryanabentivegna@gmail.com>
2019-11-02 12:41:48 -0400

    Added correct axises

--- vis.js
+++ vis.js
@@ -5,39 +5,43 @@ d3.csv("data/HackathonDataDaily.csv").then(plot)
 function plot(dataset){
 	console.log("Hello World!")
 	console.log(dataset)
-	var w = 1200
+	var w = 600
 	var h = 400
 	var padding = 40
 	
-	console.log(d3.extent(dataset, (d) => Number(d.CurrentValue)))
-	console.log([h - padding, padding])
-	
-	var xScale = d3.scaleLinear()
-		.domain([0, d3.max(dataset, (d) => Number(d.Time))])
+	var xScale = d3.scaleTime()
+		.domain(d3.extent(dataset, (d) => new Date(d.Time)))
 		.range([padding, w - padding * 2])
 	
 	var yScale = d3.scaleLinear()
 		.domain(d3.extent(dataset, (d) => Number(d.CurrentValue)))
 		.range([h - padding, padding])
 	
-	var xAxis = d3.axisBottom().scale(xScale).ticks(5)
+	var xAxis = d3.axisBottom(xScale).ticks(10)
 	
-	var yAxis = d3.axisLeft().scale(yScale).ticks(5);
-	
-	
+	var yAxis = d3.axisLeft(yScale).ticks(5)
 	
 	var svg = d3.select("body")
 		.append("svg")
 		.attr("width", w)
 		.attr("height", h)
 		
+	svg.selectAll("circle")
+		.data(dataset)
+		.enter()
+		.append("circle")
+		.attr("cx", (d) => xScale(new Date(d.Time)))
+		.attr("cy", (d) => h - yScale(Number(d.CurrentValue)))
+		.attr("r", 5)
+		.attr("fill", "green")
+		
 	svg.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + (h - padding) + ")")
-		.call(yAxis);
+		.call(xAxis)
 		
 	svg.append("g")
 		.attr("class", "y axis")
 		.attr("transform", "translate(" + padding + ", 0)")
-		.call(yAxis);
+		.call(yAxis)
 }
