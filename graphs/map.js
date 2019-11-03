ea8041493aaa91356507324736f2c52e9a0191a4
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-11-02 22:00:49 -0400

    Uploading geojson

--- graphs/map.js
+++ graphs/map.js
@@ -1,5 +1,28 @@
-d3.csv("../data/HackathonConfig.csv").then(plot)
+load()
+function load() {
+    var worldmap = d3.json("../data/data.gdb.geojson")
+    var config = d3.csv("../data/HackathonConfig.csv")
 
-function plot(dataset) {
-    console.log(dataset)
+    Promise.all([worldmap, config]).then(plot, (error) => console.log(error))
+}
+
+function plot(datasets) {
+    console.log(datasets)
+    var w = 600
+    var h = 400
+
+    var projection = d3.geoMercator()//.translate([w/2, h/2]).scale(2200).center([0,40])
+    var path = d3.geoPath().projection(projection)
+
+    var svg = d3.select("body")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h)
+
+    svg.selectAll("path")
+        .data(datasets[0].features)
+        .enter()
+        .append("path")
+        .attr("class","continent")
+        .attr("d", path)
 }
