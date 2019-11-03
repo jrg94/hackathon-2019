f17229e34196b14e7ccfffc1a3b7edc9d033c4c7
ryanbentivegna\ryan bentivegna <ryanabentivegna@gmail.com>
2019-11-02 23:07:22 -0400

    Removed the map to add to master

--- graphs/map.js
+++ graphs/map.js
@@ -2,11 +2,15 @@ load()
 function load() {
     var worldmap = d3.json("../data/data.gdb.geojson")
     var config = d3.csv("../data/HackathonConfig.csv")
+	
 
     Promise.all([worldmap, config]).then(plot, (error) => console.log(error))
 }
 
 function plot(datasets) {
+	//console.log(Math.min.apply(null, datasets[1].map((d) => Number(d.Latitude))))
+	//console.log(Math.min.apply(null, datasets[1].map((d) => Number(d.Longitude))))
+	
     console.log(datasets)
     var w = 600
     var h = 400
@@ -14,6 +18,8 @@ function plot(datasets) {
     var projection = d3.geoMercator()//.translate([w/2, h/2]).scale(2200).center([0,40])
     var path = d3.geoPath().projection(projection)
 
+	
+	
     var svg = d3.select("body")
 		.append("svg")
 		.attr("width", w)
